9e64bfef7c73f352edef61e0a1fecdb802ff069f
James <smartrejames@gmail.com>
2021-01-29 19:05:49 +0800

    support SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RxWebKit",
+    products: [
+        .library(name: "RxWebKit", targets: ["RxWebKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "RxWebKit",
+            dependencies: ["RxSwift", "RxCocoa", "RxRelay"],
+            path: "RxWebKit"
+        ),
+        .testTarget(
+            name: "RxWebKitTests",
+            dependencies: ["RxTest", "RxWebKit", "Nimble", "Quick"],
+            path: "RxWebKitTests"
+        )
+    ]
+)
